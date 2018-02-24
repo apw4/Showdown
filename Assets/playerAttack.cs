ebf29bfe2e247b329e9f2bfb9d6f802866b5620d
apw4 <36481766+apw4@users.noreply.github.com>
2018-02-24 17:03:54 -0500

    Showdown .001
    SlashFix

---
+++ Assets/playerAttack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAttack : MonoBehaviour {
+
+    private bool attacking = false;
+    private bool sheathed = true;
+    private int slashammo = 1;
+
+    public Collider2D attackTrigger;
+
+    private Animator anim;
+
+     void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        attackTrigger.enabled = false;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F) && !attacking)
+        {
+            attacking = true;
+            
+            attackTrigger.enabled = true;
+        }
+
+        if (attacking)
+        {
+            if (slashammo > 0)
+            {
+                sheathed = true;
+                slashammo -= 1;
+            }
+            else
+            {
+                attacking = false;
+                attackTrigger.enabled = false;
+                sheathed = false;
+            }
+        }
+        anim.SetBool("Attacking", attacking);
+        anim.SetBool("Sheathed", sheathed);
+    }
+}
